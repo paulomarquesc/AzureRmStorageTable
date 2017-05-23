707c23ff0b2fc85edb9900630a233d00b0b4734f
Paulo Marques <pmarques@microsoft.com>
2017-05-22 18:35:01 -0700

    Fixing a typo in error message

--- AzureRmStorageTableCoreHelper.psm1
+++ AzureRmStorageTableCoreHelper.psm1
@@ -25,7 +25,7 @@ function Test-AzureStorageTableEmptyKeys
         [String]$rowKey
 	)
     
-    $cosmosDBEmptyKeysErrorMessage = "Cosmos DB table API does not accept empty partition or row keys when using CloudTable.Execute operation, because of this we are disbling this capability in this module and it will not proceed." 
+    $cosmosDBEmptyKeysErrorMessage = "Cosmos DB table API does not accept empty partition or row keys when using CloudTable.Execute operation, because of this we are disabling this capability in this module and it will not proceed." 
 
     if ([string]::IsNullOrEmpty($partitionKey) -or [string]::IsNullOrEmpty($rowKey))
     {
