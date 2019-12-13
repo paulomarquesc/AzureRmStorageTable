c2cacdd7373e9b01f3f6c7182bd36b31068a8551
sakash279 <s.akash279@gmail.com>
2019-12-12 16:02:28 -0800

    updating tests

--- Tests/AzureRmStorageTable.Tests.ps1
+++ Tests/AzureRmStorageTable.Tests.ps1
@@ -12,6 +12,7 @@ Import-Module .\AzTable.psd1 -Force
 
 #$uniqueString = Get-Date -UFormat "PsTest%Y%m%dT%H%M%S"
 $uniqueString = [string]::Format("s{0}{1}",("{0:X}" -f (([guid]::NewGuid()).Guid.ToString().GetHashCode())).ToLower(), (Get-Date -UFormat "%Y%m%dT%H%M%S").ToString().ToLower())
+
 $resourceGroup = "$uniqueString-rg"
 
 $GetAzTableTableCmdtTableName = "TestTable"
@@ -25,40 +26,72 @@ Describe "AzureRmStorageTable" {
         }
         else
         {
-            # Storage Account
             Select-AzSubscription -SubscriptionId $SubscriptionId
             New-AzResourceGroup -Name $resourceGroup -Location $Location
+            
+            # Storage Account            
             New-AzStorageAccount -Name $uniqueString -ResourceGroupName $resourceGroup -Location $Location -SkuName Standard_LRS -Kind StorageV2
             $context = (Get-AzStorageAccount -Name $uniqueString -ResourceGroupName $resourceGroup).Context
+            
+            # CosmosDB Account
+            $apiVersion = "2015-04-08"
+            $accountProperties = @{
+                "capabilities"= @( @{ "name"="EnableTable" } ) 
+                }
+            New-AzResource -ResourceType $accountResourceType -ApiVersion $apiVersion -ResourceGroupName $resourceGroup -Location $Location -Name $uniqueString -PropertyObject $accountProperties
         }
-
+        
+        # Preparing tables for inserts, deletes and updates
         $GetTableCommand=@{$true = "-UseStorageEmulator"; $false = "-ResourceGroup `$resourceGroup -StorageAccountName `$uniqueString"}[($PSCmdlet.ParameterSetName -ne "AzureStorage")]
 
+        $GetCosmosDbTableCommand=@{$true = "-UseStorageEmulator"; $false = "-ResourceGroup `$resourceGroup -CosmosDbAccountName `$uniqueString"}[($PSCmdlet.ParameterSetName -ne "AzureStorage")]
+        
         $tables = [System.Collections.ArrayList]@()
         $tableNames = @("table$($uniqueString)insert", "table$($uniqueString)delete")
         foreach ($tableName in $tableNames)
         {
+            #Storage Test
             Write-Host -for DarkGreen "   Creating Storage Table $($tableName)"
             $Table = Invoke-Expression("Get-AzTableTable -table `$tableName $GetTableCommand")
-
             Write-Host -for Green "      Created Table $($table | out-string)"
             $tables.Add($table)
+            
+            #CosmosDB Test
+            Write-Host -for DarkGreen "   Creating CosmosDB Table $($tableName)"
+            $cosmosTable = Invoke-Expression("Get-AzTableTable -table `$tableName $GetCosmosDbTableCommand")
+            Write-Host -for Green "      Created CosmosDB Table $($table | out-string)"
+            $tables.Add($cosmosTable)
         }
     }
 
     Context "Get-AzTableTable" {
 
+        #Azure storage create and open a table
         $GetTableCommand=@{$true = "-UseStorageEmulator"; $false = "-ResourceGroup `$resourceGroup -StorageAccountName `$uniqueString"}[($PSCmdlet.ParameterSetName -ne "AzureStorage")]
-
-        It "Can create a new table" {
+        
+        It "Can create a new table(Storage account)" {
             $Table = Invoke-Expression("Get-AzTableTable -table `$GetAzTableTableCmdtTableName $GetTableCommand") 
             $Table | Should not be $null
         }
 
-        It "Can open an existing table" {
+        It "Can open an existing table (Storage account)" {
             $Table = Invoke-Expression("Get-AzTableTable -table `$GetAzTableTableCmdtTableName $GetTableCommand") 
             $Table | Should not be $null
         }
+        
+        # CosmosDB create and open a table
+        $GetCosmosDbTableCommand=@{$true = "-UseStorageEmulator"; $false = "-ResourceGroup `$resourceGroup -CosmosDbAccountName `$uniqueString"}[($PSCmdlet.ParameterSetName -ne "AzureStorage")]
+        
+        It "Can create a new table (CosmosDb account)" {
+            $Table = Invoke-Expression("Get-AzTableTable -table `$GetAzTableTableCmdtTableName $GetCosmosDbTableCommand") 
+            $Table | Should not be $null
+        }
+
+        It "Can open an existing table (CosmosDb account)" {
+            $Table = Invoke-Expression("Get-AzTableTable -table `$GetAzTableTableCmdtTableName $GetCosmosDbTableCommand") 
+            $Table | Should not be $null
+        }
+        
     }
 
     Context "Add-AzTableRow" {
@@ -136,58 +169,73 @@ Describe "AzureRmStorageTable" {
 
     Context "Get-AzTableRow" {
         BeforeAll {
-            $tableInsert = $tables | Where-Object -Property Name -EQ "table$($uniqueString)insert"
+            $tableInserts = $tables | Where-Object -Property Name -EQ "table$($uniqueString)insert"
             $partitionKey = [guid]::NewGuid().tostring()
             $rowKey = [guid]::NewGuid().tostring()
-
-            Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey $rowKey -property @{"computerName"="COMP01";"osVersion"="Windows 10";"status"="OK"}
-            Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey ([guid]::NewGuid().tostring()) -property @{"computerName"="COMP02";"osVersion"="Windows 8.1";"status"="NeedsOsUpgrade"}
-            Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey ([guid]::NewGuid().tostring()) -property @{"computerName"="COMP03";"osVersion"="Windows 8.1";"status"="NeedsOsUpgrade"}
-            Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey ([guid]::NewGuid().tostring()) -property @{"computerName"="COMP04";"osVersion"="Windows XP";"status"="NeedsOsUpgrade"}
+            
+            foreach ($tableInsert in $tableInserts)
+            {
+                Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey $rowKey -property @{"computerName"="COMP01";"osVersion"="Windows 10";"status"="OK"}
+                Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey ([guid]::NewGuid().tostring()) -property @{"computerName"="COMP02";"osVersion"="Windows 8.1";"status"="NeedsOsUpgrade"}
+                Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey ([guid]::NewGuid().tostring()) -property @{"computerName"="COMP03";"osVersion"="Windows 8.1";"status"="NeedsOsUpgrade"}
+                Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey ([guid]::NewGuid().tostring()) -property @{"computerName"="COMP04";"osVersion"="Windows XP";"status"="NeedsOsUpgrade"}
+            }
         }
 
-        It "Can it get all rows" {
-            $entityList = $null
-            $expectedRowCount = 9
-            $entityList = Get-AzTableRow -table $tableInsert
-            $entityList.Count | Should be $expectedRowCount
-        }
+        for ($i = 0; $i -lt $tableInserts.Count; $i++)
+        {
+            if ($i -eq 0)
+            {
+                $dataStore = "StorageAccount"
+            }
+            else
+            {
+                $dataStore = "CosmosDb"
+            }
+            
+            It "Can it get all rows ($dataStore)" {
+                $entityList = $null
+                $expectedRowCount = 9
+                $entityList = Get-AzTableRow -table $tableInserts[$i]
+                $entityList.Count | Should be $expectedRowCount
+            }
 
-        It "Can it get rows by partition key" {
-            $entityList = $null
-            $expectedRowCount = 4
-            $entityList = Get-AzTableRow -table $tableInsert -partitionKey $partitionKey
-            $entityList.Count | Should be $expectedRowCount
-        }
+            It "Can it get rows by partition key ($dataStore)" {
+                $entityList = $null
+                $expectedRowCount = 4
+                $entityList = Get-AzTableRow -table $tableInserts[$i] -partitionKey $partitionKey
+                $entityList.Count | Should be $expectedRowCount
+            }
 
-        It "Can it get row by partition and row key" {
-            $entityList = $null
-            $expectedRowCount = 1
-            $entityList = @(Get-AzTableRow -table $tableInsert -partitionKey $partitionKey -RowKey $rowKey)
-            $entityList.Count | Should be $expectedRowCount
-        }
+            It "Can it get row by partition and row key ($dataStore)" {
+                $entityList = $null
+                $expectedRowCount = 1
+                $entityList = @(Get-AzTableRow -table $tableInserts[$i] -partitionKey $partitionKey -RowKey $rowKey)
+                $entityList.Count | Should be $expectedRowCount
+            }
 
-        It "Can it get row by column name using guid value" {
-            $entity = $null
-            $expectedGuidValue = [guid]::NewGuid()
-            Add-AzTableRow -table $tableInsert -partitionKey $partitionKey -rowKey ([guid]::NewGuid().tostring())-property @{"computerName"="COMP05";"osVersion"="Windows 10";"status"="OK";"id"=$expectedGuidValue}
+            It "Can it get row by column name using guid value ($dataStore)" {
+                $entity = $null
+                $expectedGuidValue = [guid]::NewGuid()
+                Add-AzTableRow -table $tableInserts[$i] -partitionKey $partitionKey -rowKey ([guid]::NewGuid().tostring())-property @{"computerName"="COMP05";"osVersion"="Windows 10";"status"="OK";"id"=$expectedGuidValue}
 
-            $entity = Get-AzTableRow -Table $tableInsert -ColumnName "id" -guidvalue $expectedGuidValue -operator Equal
-            $entity.id | Should be $expectedGuidValue
-        }
+                $entity = Get-AzTableRow -Table $tableInserts[$i] -ColumnName "id" -guidvalue $expectedGuidValue -operator Equal
+                $entity.id | Should be $expectedGuidValue
+            }
 
-        It "Can it get row by column name using string value" {
-            $entity = $null
-            $expectedStringValue = "COMP02"
-            $entity = Get-AzTableRow -Table $tableInsert -ColumnName "computerName" -value $expectedStringValue -operator Equal
-            $entity.computerName | Should be $expectedStringValue
-        }
+            It "Can it get row by column name using string value ($dataStore)" {
+                $entity = $null
+                $expectedStringValue = "COMP02"
+                $entity = Get-AzTableRow -Table $tableInserts[$i] -ColumnName "computerName" -value $expectedStringValue -operator Equal
+                $entity.computerName | Should be $expectedStringValue
+            }
 
-        It "Can it get row using custom filter" {
-            $entityList = $null
-            $expectedRowCount = 1
-            $entityList = @(Get-AzTableRow -Table $tableInsert -CustomFilter "(osVersion eq 'Windows XP') and (computerName eq 'COMP04')")
-            $entityList.Count | Should be $expectedRowCount
+            It "Can it get row using custom filter ($dataStore)" {
+                $entityList = $null
+                $expectedRowCount = 1
+                $entityList = @(Get-AzTableRow -Table $tableInserts[$i] -CustomFilter "(osVersion eq 'Windows XP') and (computerName eq 'COMP04')")
+                $entityList.Count | Should be $expectedRowCount
+            }
         }
     }
 
